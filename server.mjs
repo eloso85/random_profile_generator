f07ff8702b2d83e3a9adee3bd70493ca87eef705
eloso85 <eloso85@gmail.com>
2023-11-24 17:28:14 -0600

    added about

--- server.mjs
+++ server.mjs
@@ -29,6 +29,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pages', 'about.html'));
+});
+
 // Create a route to handle API requests
 app.get('/api/data', async (req, res) => {
   try {
