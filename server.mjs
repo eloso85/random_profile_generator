0fa07912719433a9b52f2a9a9e0edc9d95a0dfff
eloso85 <eloso85@gmail.com>
2023-11-28 22:57:27 -0600

    added contact page

--- server.mjs
+++ server.mjs
@@ -29,9 +29,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/about', (req, res) => {
+app.get('/about/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'pages', 'about.html'));
 });
+
+app.get('/contact-us/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pages', 'contactUs.html'));
+})
+
+app.get('/login/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pages', 'login.html'))
+})
 
 // Create a route to handle API requests
 app.get('/api/data', async (req, res) => {
